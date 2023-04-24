f2c76ea7dbb2fb7d73944901375a9fd3b36bcc8f
cesaugusto108 <ces.augusto108@gmail.com>
2023-04-24 18:42:16 -0300

    EmployeeRepository interface REST HATEOAS resources configuration annotation

--- datarest-corp/src/main/java/augusto108/ces/datarestcorp/repository/EmployeeRepository.java
+++ datarest-corp/src/main/java/augusto108/ces/datarestcorp/repository/EmployeeRepository.java
@@ -2,6 +2,9 @@ package augusto108.ces.datarestcorp.repository;
 
 import augusto108.ces.datarestcorp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.Description;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "collaborators", collectionResourceRel = "collaborators", itemResourceRel = "collaborator")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
