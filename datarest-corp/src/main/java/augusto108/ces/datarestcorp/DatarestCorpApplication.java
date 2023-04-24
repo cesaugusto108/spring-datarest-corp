ce8f008d1a64cbb1b7abbbec9aee3baeb4596d8d
cesaugusto108 <ces.augusto108@gmail.com>
2023-04-24 17:15:10 -0300

    Project initial setup

---
+++ datarest-corp/src/main/java/augusto108/ces/datarestcorp/DatarestCorpApplication.java
@@ -0,0 +1,11 @@
+package augusto108.ces.datarestcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarestCorpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DatarestCorpApplication.class, args);
+	}
+}
---
+++ datarest-corp/src/test/java/augusto108/ces/datarestcorp/DatarestCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package augusto108.ces.datarestcorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarestCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
